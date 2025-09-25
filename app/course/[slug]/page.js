240da2cef71d779a059a39af540ef6713cb07dc7
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-09-25 14:45:17 +0600

    remove my old code

--- app/course/[slug]/page.js
+++ app/course/[slug]/page.js
@@ -500,15 +500,7 @@ const CourseDetails = async ({ params }) => {
                       <h3 className='font-bold max-w-xs text-white text-xl md:text-2xl mb-4 text-center font-siliguri'>
                         আমাদের প্ল্যাটফর্মে রয়েছে বেশ কিছু ফ্রী কোর্স মডিউল!
                       </h3>
-                      <Link
-                        href={{
-                          pathname: '/videosPlay',
-                          query: {
-                            slug: course.slug,
-                            title: course.name,
-                          },
-                        }}
-                      >
+                      <Link href={'/'}>
                         <button className='button-bg-2 rounded-md cursor-pointer px-8 py-2 whitespace-nowrap md:w-full text-white flex items-center justify-center text-md font-semibold capitalize'>
                           Watching Free Videos
                         </button>
--- app/videosPlay/page.js
+++ app/videosPlay/page.js
@@ -5,17 +5,11 @@ import {
   DefaultVideoLayout,
   defaultLayoutIcons,
 } from '@vidstack/react/player/layouts/default';
-import { Suspense } from 'react';
 
-import { useSearchParams } from 'next/navigation';
 import { useRef, useState } from 'react';
 import './VideoPlayer.css';
 
 const VideosPlaying = () => {
-  const searchParams = useSearchParams();
-  const slug = searchParams.get('slug');
-  const title = searchParams.get('title');
-
   const videoData = [
     {
       id: 1,
@@ -80,118 +74,115 @@ const VideosPlaying = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading videos...</div>}>
-      <section className='videosPlaying-section'>
-        <div className='mx-auto px-4 sm:px-6 container lg:px-8'>
-          <div className='grid grid-cols-3 gap-2 transition-all lg:gap-8'>
-            <div className='col-span-full w-full space-y-8 transition-all lg:col-span-2'>
-              <div className='flex items-center gap-2 lg:gap-4'>
-                <h4 className='font-bold text-[#124265] text-[16px] md:text-[20px] leading-[28px]'>
-                  {title} & {slug}
-                </h4>
-              </div>
-
-              <MediaPlayer
-                autoplay
-                crossorigin
-                playsinline
-                width='100%'
-                height='420px'
-                ref={videoRef}
-                title={activeVideoTitle}
-                onLoadedMetadata={handleLoadedMetadata}
-                src={videoData[currentVideoIndex].source}
-              >
-                <MediaProvider />
-                <DefaultVideoLayout
-                  thumbnails='/videos/thumbnails.vtt' // ✅ put inside /public/videos
-                  icons={defaultLayoutIcons}
-                />
-              </MediaPlayer>
-
-              <h3 className='font-semibold text-[16px] md:text-[19px] leading-[28px]'>
-                Video: {activeVideoTitle} - Duration: {videoDuration}
-              </h3>
+    <section className='videosPlaying-section'>
+      <div className='mx-auto px-4 sm:px-6 container lg:px-8'>
+        <div className='grid grid-cols-3 gap-2 transition-all lg:gap-8'>
+          <div className='col-span-full w-full space-y-8 transition-all lg:col-span-2'>
+            <div className='flex items-center gap-2 lg:gap-4'>
+              <h4 className='font-bold text-[#124265] text-[16px] md:text-[20px] leading-[28px]'>
+                {title} & {slug}
+              </h4>
             </div>
 
-            <div className=' shadow-1 col-span-full h-max rounded-md   lg:col-span-1 lg:rounded-lg'>
-              <div className='bg-[#e5e7eb] custom-shadow px-4 py-7 rounded-lg '>
-                <div className='videos-module '>
-                  <div className='Course-Content flex items-center justify-between pb-4'>
-                    <h4 className='font-semibold text-[16px] md:text-[20px] '>
-                      Course Content
-                    </h4>
-                    <h5 className='font-semibold text-[16px] md:text-[20px] text-primary'>
-                      {videoData.length + 4} Lessons (8h 15m)
-                    </h5>
-                  </div>
+            <MediaPlayer
+              autoplay
+              crossorigin
+              playsinline
+              width='100%'
+              height='420px'
+              ref={videoRef}
+              title={activeVideoTitle}
+              onLoadedMetadata={handleLoadedMetadata}
+              src={videoData[currentVideoIndex].source}
+            >
+              <MediaProvider />
+              <DefaultVideoLayout
+                thumbnails='/videos/thumbnails.vtt' // ✅ put inside /public/videos
+                icons={defaultLayoutIcons}
+              />
+            </MediaPlayer>
 
-                  <details
-                    className='group rounded-lg [&_summary::-webkit-details-marker]:hidden mb-[1rem] bg-white '
-                    open
-                  >
-                    <summary className='flex items-center justify-between cursor-pointer bg-white hover:bg-[#f0f8ff] py-[1rem] px-[1.25rem] rounded-[6px]'>
-                      <h2 className='font-bold text-[#124265] text-[14px] md:text-[16px]'>
-                        Oracle Database-SQL: Practice SQL to be confident in
-                        Query{' '}
-                        <span className='font-medium text-[#7b7b8a]'>
-                          ({videoData.length} Lectures)
-                        </span>
-                      </h2>
-                    </summary>
+            <h3 className='font-semibold text-[16px] md:text-[19px] leading-[28px]'>
+              Video: {activeVideoTitle} - Duration: {videoDuration}
+            </h3>
+          </div>
 
-                    <div className='mt-[1rem] pl-[1rem] pt-[0.2rem] pr-[0.5rem] pb-[0.7rem]'>
-                      {videoData.map((data, index) => (
-                        <div key={data.id}>
-                          <div className='flex justify-between items-center flex-wrap'>
-                            <div className='relative flex items-center videos-name-main flex-wrap'>
-                              <a
-                                href='#'
-                                onClick={() => changeVideoSource(data.id - 1)}
-                                className='btn-danger-soft mb-0'
-                              >
-                                <svg
-                                  xmlns='http://www.w3.org/2000/svg'
-                                  width='23'
-                                  height='23'
-                                  fill='currentColor'
-                                  className='bi bi-play-fill play-icons mr-0 pl-[3px]'
-                                  viewBox='0 0 16 16'
-                                >
-                                  <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
-                                </svg>
-                              </a>
-                              <span className='inline-block truncate ml-2 mb-0 text-[15px] font-normal w-[280px] cursor-pointer'>
-                                <span
-                                  className={`${
-                                    currentVideoIndex === index
-                                      ? 'active-video text-[#d6293e] font-semibold text-[15px]'
-                                      : ''
-                                  }`}
-                                  onClick={() => changeVideoSource(data.id - 1)}
-                                >
-                                  {data.title}
-                                </span>
-                              </span>
-                            </div>
-                            <p className='mb-0 text-[#7b7b8a] text-[14px] font-semibold flex items-center gap-2'>
-                              {videoDuration}
-                            </p>
-                          </div>
-                          {index !== videoData.length - 1 && (
-                            <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
-                          )}
-                        </div>
-                      ))}
-                    </div>
-                  </details>
+          <div className=' shadow-1 col-span-full h-max rounded-md   lg:col-span-1 lg:rounded-lg'>
+            <div className='bg-[#e5e7eb] custom-shadow px-4 py-7 rounded-lg '>
+              <div className='videos-module '>
+                <div className='Course-Content flex items-center justify-between pb-4'>
+                  <h4 className='font-semibold text-[16px] md:text-[20px] '>
+                    Course Content
+                  </h4>
+                  <h5 className='font-semibold text-[16px] md:text-[20px] text-primary'>
+                    {videoData.length + 4} Lessons (8h 15m)
+                  </h5>
                 </div>
+
+                <details
+                  className='group rounded-lg [&_summary::-webkit-details-marker]:hidden mb-[1rem] bg-white '
+                  open
+                >
+                  <summary className='flex items-center justify-between cursor-pointer bg-white hover:bg-[#f0f8ff] py-[1rem] px-[1.25rem] rounded-[6px]'>
+                    <h2 className='font-bold text-[#124265] text-[14px] md:text-[16px]'>
+                      Oracle Database-SQL: Practice SQL to be confident in Query{' '}
+                      <span className='font-medium text-[#7b7b8a]'>
+                        ({videoData.length} Lectures)
+                      </span>
+                    </h2>
+                  </summary>
+
+                  <div className='mt-[1rem] pl-[1rem] pt-[0.2rem] pr-[0.5rem] pb-[0.7rem]'>
+                    {videoData.map((data, index) => (
+                      <div key={data.id}>
+                        <div className='flex justify-between items-center flex-wrap'>
+                          <div className='relative flex items-center videos-name-main flex-wrap'>
+                            <a
+                              href='#'
+                              onClick={() => changeVideoSource(data.id - 1)}
+                              className='btn-danger-soft mb-0'
+                            >
+                              <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='23'
+                                height='23'
+                                fill='currentColor'
+                                className='bi bi-play-fill play-icons mr-0 pl-[3px]'
+                                viewBox='0 0 16 16'
+                              >
+                                <path d='m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393' />
+                              </svg>
+                            </a>
+                            <span className='inline-block truncate ml-2 mb-0 text-[15px] font-normal w-[280px] cursor-pointer'>
+                              <span
+                                className={`${
+                                  currentVideoIndex === index
+                                    ? 'active-video text-[#d6293e] font-semibold text-[15px]'
+                                    : ''
+                                }`}
+                                onClick={() => changeVideoSource(data.id - 1)}
+                              >
+                                {data.title}
+                              </span>
+                            </span>
+                          </div>
+                          <p className='mb-0 text-[#7b7b8a] text-[14px] font-semibold flex items-center gap-2'>
+                            {videoDuration}
+                          </p>
+                        </div>
+                        {index !== videoData.length - 1 && (
+                          <hr className='hr-style my-[1rem] text-[#9a9ea4]' />
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </details>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </Suspense>
+      </div>
+    </section>
   );
 };
 
